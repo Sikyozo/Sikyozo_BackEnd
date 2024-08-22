92dda17e0ce37571b22420349552a8588b0924c9
yezy <iyeahji@gmail.com>
2024-08-22 16:56:29 +0900

    init commit

---
+++ src/main/java/com/spring/sikyozo/SikyozoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sikyozo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikyozoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikyozoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sikyozo/SikyozoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sikyozo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikyozoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
