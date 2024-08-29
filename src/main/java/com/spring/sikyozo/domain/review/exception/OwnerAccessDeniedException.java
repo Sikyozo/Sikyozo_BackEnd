e196f4c8e9aaa934432cc9b0bd8facab79f3d9cb
rkoji <wl0wn1212@gmail.com>
2024-08-29 20:47:00 +0900

    [Feat] 리뷰 Exception 추가 - 평점 범위를 벗어난 에러 추가 - 가게 주인 유저는 사용하지 못하도록 에러 추가

---
+++ src/main/java/com/spring/sikyozo/domain/review/exception/OwnerAccessDeniedException.java
@@ -0,0 +1,12 @@
+package com.spring.sikyozo.domain.review.exception;
+
+import com.spring.sikyozo.global.exception.ErrorCode;
+import com.spring.sikyozo.global.exception.SikyozoException;
+import com.spring.sikyozo.global.exception.domainErrorCode.ReviewErrorCode;
+
+public class OwnerAccessDeniedException extends SikyozoException {
+
+    public OwnerAccessDeniedException() {
+        super(ReviewErrorCode.OWNER_ACCESS_DENIED);
+    }
+}
