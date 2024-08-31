db0084a3e03fda34e4db3fba6d7a577c995289a0
shoon95 <tkdgns5817@gmail.com>
2024-09-01 07:59:14 +0900

    [Comment] 로그 추가
    - RedisCartService 로그 추가

--- src/main/java/com/spring/sikyozo/domain/cart/service/RedisCartService.java
+++ src/main/java/com/spring/sikyozo/domain/cart/service/RedisCartService.java
@@ -2,11 +2,17 @@ package com.spring.sikyozo.domain.cart.service;
 
 import com.spring.sikyozo.domain.cart.dto.response.GetCartResponseDto;
 import com.spring.sikyozo.domain.cart.entity.CartItem;
+import com.spring.sikyozo.domain.cart.exception.CartItemCannotAddedOrUpdated;
 import com.spring.sikyozo.domain.cart.exception.DifferentStoreException;
-import com.spring.sikyozo.domain.cart.exception.MenuDoesNotExistsException;
 import com.spring.sikyozo.domain.cart.repository.CartRepository;
 import com.spring.sikyozo.domain.menu.entity.Menu;
+import com.spring.sikyozo.domain.menu.exception.MenuNotFoundException;
 import com.spring.sikyozo.domain.menu.repository.MenuRepositoryImpl;
+import com.spring.sikyozo.domain.store.entity.Store;
+import com.spring.sikyozo.domain.user.entity.User;
+import com.spring.sikyozo.domain.user.entity.UserRole;
+import com.spring.sikyozo.domain.user.exception.UserNotFoundException;
+import com.spring.sikyozo.domain.user.repository.UserRepository;
 import com.spring.sikyozo.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,35 +29,64 @@ public class RedisCartService implements CartService {
 
     private final MenuRepositoryImpl menuRepository;
     private final CartRepository cartRepository;
+    private final UserRepository userRepository;
 
     /**
      * 장바구니에 품목 추가 또는 수량 수정하기
      */
     @Override
-    public void addOrUpdateCartItem(Long userId, UUID menuId, Integer quantity) {
+    public void addOrUpdateCartItem(Long loginUserId, UUID menuId, Integer quantity) {
 
-        CartItem cartItem = cartRepository.getCartItemByUserIdAndMenuId(userId, menuId);
+        log.info("장바구니 상품 추가, 수정 요청 loginUserId: {}, menuId: {}, quantity: {}", loginUserId, menuId, quantity);
+
+        User user = findUserById(loginUserId);
+        CartItem cartItem = cartRepository.getCartItemByUserIdAndMenuId(loginUserId, menuId);
+
+        checkUserRoleForCart(user);
+
 
         // 장바구니에 이미 품목이 있으면 수량만 수정
         if (cartItem != null) {
-            log.info("장바구니 수량 변경 userId: {}, menuId: {}, quantity: {}", userId, menuId, quantity);
+            log.info("장바구니 수량 변경 userId: {}, menuId: {}, quantity: {}", loginUserId, menuId, quantity);
             cartItem.updateQuantity(quantity);
         } else {
-            log.info("장바구니 품목 추가 userId: {}, menuId: {}, quantity: {}", userId, menuId, quantity);
+            log.info("장바구니 품목 추가 userId: {}, menuId: {}, quantity: {}", loginUserId, menuId, quantity);
 
             // 추가하는 품목이 기존 장바구니에 있는 품목과 같은 Store의 품목인지 확인
             Menu menuById = getMenuById(menuId);
 
-            if (cartRepository.isCartNotEmpty(userId)) {
-                verifySameStore(userId, menuById);
+            // 가게 주인의 경우 본인 가게 메뉴인지 확인
+            Store store = menuById.getStore();
+            checkOwnerStore(user, store);
+
+            if (cartRepository.isCartNotEmpty(loginUserId)) {
+                verifySameStore(loginUserId, menuById);
             }
             cartItem = CartItem.create(menuById, quantity);
         }
-        cartRepository.save(userId, menuId, cartItem);
+
+        log.info("장바구니 상품 추가, 수정 요청 loginUserId: {}, menuId: {}, quantity: {}", loginUserId, menuId, quantity);
+
+        cartRepository.save(loginUserId, menuId, cartItem);
+    }
+
+    private User findUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("유저를 찾을 수 없습니다."));
+    }
+
+    private void checkUserRoleForCart(User user) {
+        if (user.getRole().equals(UserRole.MASTER) || user.getRole().equals(UserRole.MANAGER)) {
+            throw new CartItemCannotAddedOrUpdated("장바구니 권한이 없습니다.");
+        }
+    }
+    private void checkOwnerStore(User user, Store store) {
+        if (user.getRole().equals(UserRole.OWNER) && !user.getStores().contains(store)) {
+            throw new CartItemCannotAddedOrUpdated("본인의 가게 상품만 담을 수 있습니다.");
+        }
     }
 
     private Menu getMenuById(UUID menuId) {
-        return menuRepository.findById(menuId).orElseThrow(() -> new MenuDoesNotExistsException("메뉴가 존재하지 않습니다."));
+        return menuRepository.findById(menuId).orElseThrow(() -> new MenuNotFoundException("메뉴가 존재하지 않습니다."));
     }
 
     private void verifySameStore(Long userId, Menu menuById) {
@@ -60,6 +95,7 @@ public class RedisCartService implements CartService {
 
         // 다른 가게의 상품이면 예외 발생
         if (!menuById.getStore().getId().toString().equals(itemFromRedis.getStoreId())) {
+            log.warn("정책 문제로(동일 가게 상품만 가능) 장바구니 물건 추가, 수정 요청 실패 loginUserId: {}, menuId: {}", userId, menuById);
             throw new DifferentStoreException("동일한 가게의 상품만 담을 수 있습니다.");
         }
     }
@@ -68,14 +104,21 @@ public class RedisCartService implements CartService {
      * 장바구니 전체 조회하기
      */
     @Override
-    public ResponseDto<List<GetCartResponseDto>> getCart(Long userId) {
+    public ResponseDto<List<GetCartResponseDto>> getCart(Long loginUserId) {
 
-        log.info("장바구니 조회 userId: {}", userId);
+        log.info("장바구니 조회 요청 loginUserId: {}", loginUserId);
 
-        List<CartItem> cartItemList = cartRepository.getCartItemsByUserId(userId);
+        User user = findUserById(loginUserId);
+        checkUserRoleForCart(user);
+        log.info("장바구니 조회 userId: {}", loginUserId);
+
+        List<CartItem> cartItemList = cartRepository.getCartItemsByUserId(loginUserId);
         List<GetCartResponseDto> getCartResponseDtoList = cartItemList.stream()
                 .map(m -> new GetCartResponseDto(m.getId(), m))
                 .collect(Collectors.toList());
+
+        log.info("장바구니 조회 요청 성공 loginUserId: {}", loginUserId);
+
         return ResponseDto.success("장바구니 조회에 성공하였습니다", getCartResponseDtoList);
     }
 
@@ -84,11 +127,16 @@ public class RedisCartService implements CartService {
      * 장바구니에서 품목 제거
      */
     @Override
-    public UUID removeItemFromCart(Long userId, UUID menuId) {
+    public UUID removeItemFromCart(Long loginUserId, UUID menuId) {
 
-        log.info("장바구니에서 품목 제거 userId: {}, menuId: {}", userId, menuId);
+        log.info("장바구니에서 품목 제거 요청 userId: {}, menuId: {}", loginUserId, menuId);
 
-        cartRepository.removeItemByUserIdAndMenuId(userId, menuId);
+        User user = findUserById(loginUserId);
+        checkUserRoleForCart(user);
+
+        cartRepository.removeItemByUserIdAndMenuId(loginUserId, menuId);
+
+        log.info("장바구니 조회 요청 성공 loginUserId: {}, menuId: {}", loginUserId, menuId);
 
         return menuId;
     }
@@ -97,11 +145,16 @@ public class RedisCartService implements CartService {
      * 장바구니 비우기
      */
     @Override
-    public void clearCart(Long userId) {
+    public void clearCart(Long loginUserId) {
 
-        log.info("장바구니 비우기 userId: {}", userId);
+        log.info("장바구니 비우기 요청 userId: {}", loginUserId);
 
-        cartRepository.deleteCartByUserId(userId);
+        User user = findUserById(loginUserId);
+        checkUserRoleForCart(user);
+
+        log.info("장바구니 비우기 성공 userId: {}", loginUserId);
+
+        cartRepository.deleteCartByUserId(loginUserId);
     }
 
 }
