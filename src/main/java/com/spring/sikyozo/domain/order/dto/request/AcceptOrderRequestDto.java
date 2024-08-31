21b5b3281570128b3af5fb13399e92b86f5fb3da
shoon95 <tkdgns5817@gmail.com>
2024-09-01 08:14:10 +0900

    [Feat] 주문 수락 요청 dto 생성

---
+++ src/main/java/com/spring/sikyozo/domain/order/dto/request/AcceptOrderRequestDto.java
@@ -0,0 +1,10 @@
+package com.spring.sikyozo.domain.order.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AcceptOrderRequestDto {
+    private Long userId;
+}
