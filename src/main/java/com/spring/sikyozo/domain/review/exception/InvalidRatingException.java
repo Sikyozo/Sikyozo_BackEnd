f1dc0f43423ba9cde374525f6eafd35fe67d51ba
rkoji <wl0wn1212@gmail.com>
2024-08-31 20:27:25 +0900

    [Refactor] 잠시 사용하지 않는 exception 주석

--- src/main/java/com/spring/sikyozo/domain/review/exception/InvalidRatingException.java
+++ src/main/java/com/spring/sikyozo/domain/review/exception/InvalidRatingException.java
@@ -1,11 +1,11 @@
-package com.spring.sikyozo.domain.review.exception;
-
-import com.spring.sikyozo.global.exception.ErrorCode;
-import com.spring.sikyozo.global.exception.SikyozoException;
-import com.spring.sikyozo.global.exception.domainErrorCode.ReviewErrorCode;
-
-public class InvalidRatingException extends SikyozoException {
-    public InvalidRatingException() {
-        super(ReviewErrorCode.INVALID_RATING);
-    }
-}
+//package com.spring.sikyozo.domain.review.exception;
+//
+//import com.spring.sikyozo.global.exception.ErrorCode;
+//import com.spring.sikyozo.global.exception.SikyozoException;
+//import com.spring.sikyozo.global.exception.domainErrorCode.ReviewErrorCode;
+//
+//public class InvalidRatingException extends SikyozoException {
+//    public InvalidRatingException() {
+//        super(ReviewErrorCode.INVALID_RATING);
+//    }
+//}
