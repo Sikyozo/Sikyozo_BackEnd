e2437424348ab4bf9bb3368a1d073ef2dbe00df9
shoon95 <tkdgns5817@gmail.com>
2024-09-02 19:30:26 +0900

    [Fix] redis 연결 정보 수

--- src/main/java/com/spring/sikyozo/global/redis/RedisConfig.java
+++ src/main/java/com/spring/sikyozo/global/redis/RedisConfig.java
@@ -1,10 +1,8 @@
 package com.spring.sikyozo.global.redis;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -20,21 +18,10 @@ public class RedisConfig extends AbstractHttpSessionApplicationInitializer {
         super(RedisConfig.class);
     }
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
 
         // 일반적인 key:value의 경우 Serializer
         redisTemplate.setKeySerializer(new StringRedisSerializer());
