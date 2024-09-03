73ce7113c12f8d0e9e00b0949573db2b18b3bf0a
shoon95 <tkdgns5817@gmail.com>
2024-09-03 11:37:43 +0900

    [chore] 초기 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- User 데이터 삽입
 INSERT INTO p_users (id, username, nickname, email, password, role, created_at) VALUES
-                                                                                    (1, 'john_doe', 'johnny', 'john@example.com', 'hashed_password1', 'CUSTOMER', NOW()),
+--                                                                                     (1, 'john_doe', 'johnny', 'john@example.com', 'hashed_password1', 'CUSTOMER', NOW()),
                                                                                     (2, 'jane_smith', 'janes', 'jane@example.com', 'hashed_password2', 'OWNER', NOW()),
                                                                                     (3, 'alice_wonder', 'alice', 'alice@example.com', 'hashed_password3', 'CUSTOMER', NOW()),
                                                                                     (4, 'bob_builder', 'bob', 'bob@example.com', 'hashed_password4', 'MANAGER', NOW()),
@@ -39,7 +39,7 @@ INSERT INTO p_menus (id, store_id, menu_name, price, menu_img, created_at, hidde
                                                                                        ('eeee1111-1111-1111-1111-111111111111', 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee', 'Magic Tea', 5000, 'magic_tea_image_url', NOW(), false);
 -- Address 데이터 삽입
 INSERT INTO p_addresses (id, user_id, address_name, request, created_at) VALUES
-                                                                             ('aaaa0000-0000-0000-0000-000000000001', 1, '123 Main St, Springfield', 'Leave at the door', NOW()),
+--                                                                              ('aaaa0000-0000-0000-0000-000000000001', 1, '123 Main St, Springfield', 'Leave at the door', NOW()),
                                                                              ('aaaa0000-0000-0000-0000-000000000002', 2, '456 Elm St, Metropolis', 'Ring the bell', NOW()),
                                                                              ('aaaa0000-0000-0000-0000-000000000003', 3, '789 Oak St, Gotham City', 'Please be quick', NOW()),
                                                                              ('aaaa0000-0000-0000-0000-000000000004', 4, '101 Maple St, Springfield', 'Call on arrival', NOW()),
