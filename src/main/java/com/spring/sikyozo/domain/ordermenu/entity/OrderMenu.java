d15eeb16acb4bc688c6f7b8158b60ba340afdef5
shoon95 <tkdgns5817@gmail.com>
2024-09-03 11:36:30 +0900

    [Fix] 주문 조회를 위한 Dto 생

--- src/main/java/com/spring/sikyozo/domain/ordermenu/entity/OrderMenu.java
+++ src/main/java/com/spring/sikyozo/domain/ordermenu/entity/OrderMenu.java
@@ -3,11 +3,13 @@ package com.spring.sikyozo.domain.ordermenu.entity;
 import com.spring.sikyozo.domain.menu.entity.Menu;
 import com.spring.sikyozo.domain.order.entity.Order;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.util.UUID;
 
 @Entity
 @Table(name="p_order_menus")
+@Getter
 public class OrderMenu {
 
     @Id
